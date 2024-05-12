69af7d1289b7bde0b6bdc1478e8b0f50a92e8eec
Mohib <mohibullahmohim2020@gmail.com>
2024-05-12 14:44:38 +0600

    delete data calling

--- index.js
+++ index.js
@@ -74,6 +74,13 @@ async function run() {
       const result = await bookedCollection.insertOne(bookData) ;
       res.send(result)
     })
+    // delete data from manage router
+    app.delete('/bookedServices/:id',async(req,res) => {
+      const id = req.params.id ;
+      const query = {_id : new ObjectId(id)} ;
+      const result = await bookedCollection.deleteOne(query);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
